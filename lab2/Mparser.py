452a9ee06e700bca4376cdada2d97cd35844e10c
uqsz <uqsz123@gmail.com>
2023-10-16 17:15:39 +0200

    lab1

---
+++ lab2/Mparser.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import scanner
+import ply.yacc as yacc
+
+
+tokens = scanner.tokens
+
+precedence = (
+   # to fill ...
+   ("left", '+', '-'),
+   # to fill ...
+)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}: LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_program(p):
+    """program : instructions_opt"""
+
+def p_instructions_opt_1(p):
+    """instructions_opt : instructions """
+
+def p_instructions_opt_2(p):
+    """instructions_opt : """
+
+def p_instructions_1(p):
+    """instructions : instructions instruction """
+
+def p_instructions_2(p):
+    """instructions : instruction """
+
+# to finish the grammar
+# ....
+
+
+    
+
+
+parser = yacc.yacc()
+

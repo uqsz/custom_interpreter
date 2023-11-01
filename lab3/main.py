36eb1ba94b75407c8d713b4ff4fe3d953b81a338
uqsz <uqsz123@gmail.com>
2023-11-01 19:46:57 +0100

    lab3

--- lab3/main.py
+++ lab3/main.py
@@ -8,7 +8,8 @@ from TreePrinter import TreePrinter
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] if len(
+            sys.argv) > 1 else "lab3/examples/example1.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
